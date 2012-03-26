dcce50921a43bbe7a30019c2d6cb061f531c19b3
Chris Toynbee <ctoynbee@gmail.com>
2012-03-27 11:57:34 +1300

    Add require to fix missing route
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'trackable_items'
+# Routes needs more thought since some default routes in ArchivesCentral are taking precedence.
 ActionController::Routing::Routes.draw do |map|
   map.resources :tracking_lists, :only => [:show]
 end

31b29d0d687b95321306192c152fdb64ad0a63e7
Chris Toynbee <ctoynbee@gmail.com>
2012-03-26 13:46:50 +1300

    Basic Tracking List Controller
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

---
+++ app/controllers/tracking_lists_controller.rb
@@ -0,0 +1,20 @@
+class TrackingListsController < ApplicationController
+  # Prevents the following error from showing up, common in Rails engines
+  # A copy of ApplicationController has been removed from the module tree but is still active!
+  unloadable
+  
+  before_filter :get_trackable_items_user
+  before_filter :get_tracking_list
+  
+  
+  protected
+
+  def get_trackable_items_user
+    @trackable_items_user = User.find(params[:user_id])
+  end
+
+  def get_tracking_list
+    @tracking_list = @trackable_items_user.tracking_list
+  end
+  
+end

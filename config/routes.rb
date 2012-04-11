b4b28b977798276033eb9fc5cc17411f3a60ff65
Chris Toynbee <ctoynbee@gmail.com>
2012-04-11 14:13:07 +1200

    Incomplete repository work for viewing
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 require 'trackable_items'
 # Routes needs more thought since some default routes in ArchivesCentral are taking precedence.
 ActionController::Routing::Routes.draw do |map|
-  map.resources :tracking_lists, :only => [:show]
+  map.resources :tracking_lists, :only => [:show, :index]
+  map.resources :repositories
+  map.resources :shelf_locations
 end

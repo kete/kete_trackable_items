5284512c5470a68342e4855c53af22c79c836d33
Chris Toynbee <ctoynbee@gmail.com>
2012-04-27 12:05:37 +1200

    Set up locales for use in Archives Central
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- rails/init.rb
+++ rails/init.rb
@@ -1,2 +1,7 @@
 ActionController::Base.send(:include, KeteTrackableItemsControllerHelpers)
 ActionController::Base.send(:helper, KeteTrackableItemsHelper)
+
+config.to_prepare do
+  # load our locales
+  I18n.load_path += Dir[ File.join(File.dirname(__FILE__), '../config/locales/*.{rb,yml}') ]
+end

b0078b09d96200360088ec45b82eaf15301462e2
Chris Toynbee <ctoynbee@gmail.com>
2012-05-01 17:30:51 +1200

    Moved associations to trackable_items.rb to DRY up
    Also pluralised through association
    
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- app/models/shelf_location.rb
+++ app/models/shelf_location.rb
@@ -8,7 +8,6 @@ class ShelfLocation < ActiveRecord::Base
   belongs_to :repository
   # you might be able to leave off the :polymorphic => true bit,
   # though you might have to specify :source => :trackable_item perhaps
-  has_many :trackable_item_shelf_locations
-  has_many :trackable_items, :through => :trackable_item_shelf_locations
-  # or has_many :trackable_item_shelf_locations
+  has_many :trackable_items_shelf_locations
+  has_many :trackable_items, :through => :trackable_items_shelf_locations
 end
--- lib/kete_trackable_items/extensions/models/topic.rb
+++ lib/kete_trackable_items/extensions/models/topic.rb
@@ -4,7 +4,4 @@ Topic.class_eval do
   include TrackableItems
   set_up_trackable_items :described_as => :reference_title_date_range
 
-  has_many :shelf_locations, :through => :trackable_item_shelf_location
-  #has_many :trackable_items_shelf_locations
-
 end

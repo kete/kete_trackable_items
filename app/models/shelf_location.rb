11349353da587b19c0359b2a6b067078e3e1f1ee
Walter McGinnis <wm@waltermcginnis.com>
2012-05-01 13:05:13 +1200

    Cleaning up and adding missing has_many.

--- app/models/shelf_location.rb
+++ app/models/shelf_location.rb
@@ -8,6 +8,7 @@ class ShelfLocation < ActiveRecord::Base
   belongs_to :repository
   # you might be able to leave off the :polymorphic => true bit,
   # though you might have to specify :source => :trackable_item perhaps
-  has_many :trackable_items, :through => :trackable_item_shelf_location, :polymorphic => true
+  has_many :trackable_item_shelf_locations
+  has_many :trackable_items, :through => :trackable_item_shelf_locations
   # or has_many :trackable_item_shelf_locations
 end

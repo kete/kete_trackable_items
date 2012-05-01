65b6f5691329f4bd5ba5dc8a0e886097418cf089
Chris Toynbee <ctoynbee@gmail.com>
2012-05-01 15:05:44 +1200

    Add missing controller in class name.
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- app/controllers/trackable_items_shelf_locations_controller.rb
+++ app/controllers/trackable_items_shelf_locations_controller.rb
@@ -1,4 +1,4 @@
-class TrackableItemsShelfLocations < ApplicationController
+class TrackableItemsShelfLocationsController < ApplicationController
   # Prevents the following error from showing up, common in Rails engines
   # A copy of ApplicationController has been removed from the module tree but is still active!
   unloadable

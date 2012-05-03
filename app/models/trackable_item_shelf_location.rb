7b992c6b245e31968d9ab1043752a139a872da60
Chris Toynbee <ctoynbee@gmail.com>
2012-05-03 12:55:23 +1200

    Singularise class
    Signed-off-by: Chris Toynbee <ctoynbee@gmail.com>

--- app/models/trackable_item_shelf_location.rb
+++ app/models/trackable_item_shelf_location.rb
@@ -1,4 +1,4 @@
-class TrackableItemsShelfLocations < ActiveRecord::Base
+class TrackableItemShelfLocation < ActiveRecord::Base
   belongs_to :shelf_location
   belongs_to :trackable_item, :polymorphic => true
 end

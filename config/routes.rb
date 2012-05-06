c28040f832d4b19e3b60a4f90d5f94c0aa44045f
Walter McGinnis <wm@waltermcginnis.com>
2012-05-06 16:29:16 +1200

    Taking out require that isn't necessary.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-require 'trackable_items'
 # Routes needs more thought since some default routes in ArchivesCentral are taking precedence.
 ActionController::Routing::Routes.draw do |map|
   map.resources :tracking_lists, :path_prefix => ':urlified_name', :only => [:show, :new, :index]

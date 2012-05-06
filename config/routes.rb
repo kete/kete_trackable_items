48cefd8cdc04561ed79ffc1c931014bca330e511
Walter McGinnis <wm@waltermcginnis.com>
2012-05-06 19:22:23 +1200

    Adding tracked_items#create and trackable_item_shelf_locations#create routes. These may expand later.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # Routes needs more thought since some default routes in ArchivesCentral are taking precedence.
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tracked_items, :path_prefix => ':urlified_name', :only => [:create]
+  map.resources :trackable_item_shelf_locations, :path_prefix => ':urlified_name', :only => [:create]
   map.resources :tracking_lists, :path_prefix => ':urlified_name', :only => [:show, :new, :index]
   map.resources :repositories, :path_prefix => ':urlified_name'
   map.resources :shelf_locations, :path_prefix => ':urlified_name'

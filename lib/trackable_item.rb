b583783472e8a4a814b8a896b7da78f83608ede7
Walter McGinnis <wm@waltermcginnis.com>
2012-05-08 19:21:43 +1200

    Cleaning out stub code that isn't likely to be used. Fixing odds and ends, putting in comment. Interim step. Workflow specification likely to be moved.

--- lib/trackable_item.rb
+++ lib/trackable_item.rb
@@ -1,15 +1,5 @@
 require "active_record"
 
-# Attributes
-# box_number
-# description
-# state
-# series_number
-# current_location
-
-# possible states:
-# => allocated, display, held_out, on_loan_organisation,  (specified from external data source?)
-
 module TrackableItem
   def self.included(base)
     base.extend(ClassMethods)
@@ -51,7 +41,6 @@ module TrackableItem
         end
       end
     end
-
   end
 
   module ClassMethods
@@ -66,39 +55,13 @@ module TrackableItem
       # May be necessary: , :dependent => :delete_all
       send :has_many, :trackable_item_shelf_locations, :as => :trackable_item, :dependent => :delete_all
       send :has_many, :shelf_locations, :through => :trackable_item_shelf_locations
-      #acts_as :trackable_item
 
       # not sure if it has many/what about subitems?
       send :has_one, :on_loan_organisations
 
       # set up the ability to be added to a tracking list, etc.
-      has_many :tracked_items, :as => :trackable_item, :dependent => :delete_all
+      send :has_many, :tracked_items, :as => :trackable_item, :dependent => :delete_all
       send :has_many, :tracking_lists, :through => :tracked_items
-
-      def description_for_tracked_item
-
-      end
-
-      # Define workflow methods
-      def allocated
-
-      end
-
-      def display
-
-      end
-
-      def held_out
-
-      end
-
-      def on_loan_organisation
-
-      end
-
-      def to_be_refiled
-
-      end
     end
   end
 
@@ -107,6 +70,8 @@ module TrackableItem
     def has_shelf_location?
       shelf_locations.size > 1
     end
-
+    
+    # TODO: add workflow triggered methods that run when corresponding event is triggered
+    # as necessary
   end
 end

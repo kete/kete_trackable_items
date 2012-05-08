97ff7bae5c86911b737bc8451442eafa0e6d6791
Walter McGinnis <wm@waltermcginnis.com>
2012-05-08 13:21:46 +1200

    Cleaning out cruft that is not needed.

--- app/controllers/tracked_items_controller.rb
+++ app/controllers/tracked_items_controller.rb
@@ -3,44 +3,14 @@ class TrackedItemsController < ApplicationController
   # A copy of ApplicationController has been removed from the module tree but is still active!
   unloadable
   
-  ##########
-  # NOTES FOR CHRIS
-  # missing link was adding this to Topic in archives central
-  #    has_many :tracked_items, :as => :trackable_item, :dependent => :destroy
-  # if you look at trollied in lib/gets_trollied`
-  # there is this bit of magic
-
-  # module ClassMethods
-  #   def set_up_to_get_trollied(*args)
-
-  #     send :has_many, :line_items, :as => :purchasable_item, :dependent => :destroy
-  # thats where all the models become purchaseable_items
-  # guessing we will need something like that in the trackable items gem as well
-  # once we have a trackable_item adding and removing things from the list is not so bad.
-
-  # I did this in the console
-  # >> t = Topic.last
-  # >> tl = TrackingList.last
-  # >> tracked_item = t.tracked_items.build :tracking_list => tl
-  # >> tracked_item.save
-  # => true
-  # >> TrackedItem.count
-  # => 1
-  # >> z = t.tracked_items.find_by_tracking_list_id tl
-  # => #<TrackedItem id: 3, tracking_list_id: 3, trackable_item_id: 106775, trackable_item_type: "Topic", created_at: "2012-05-04 05:25:07", updated_at: "2012-05-04 05:25:07">
-  # >> z.destroy
-  # >> TrackedItem.count
-  # => 0
-
   before_filter :get_trackable_item
   before_filter :get_tracking_list
-  before_filter :get_user
   
   def create
     tracked_item = @trackable_item.tracked_items.build(@tracking_list)
 
     if tracked_item.save
-      redirect_to url_for_tracking_list(@tracking_list.id)
+      redirect_to tracking_list_url(:id => @tracking_list)
     end
   end
 
@@ -48,9 +18,12 @@ class TrackedItemsController < ApplicationController
   def destroy
     tracked_item = @trackable_item.tracked_items.find_by_tracking_list_id(@tracking_list)
     tracked_item.destroy unless tracked_item.nil?
+
+    # TODO: this redirect may need to redirect a different place in some contexts
+    redirect_to tracking_list_url(:id => @tracking_list)
   end
 
-  protected
+  private
   
   def get_trackable_item
     type = params[:tracked_item_type]
@@ -59,9 +32,5 @@ class TrackedItemsController < ApplicationController
   
   def get_tracking_list
     @tracking_list ||= TrackingList.find_by_id(params[:tracking_list_id])
-  end
-    
-  def get_user
-    @user = current_user
   end
 end

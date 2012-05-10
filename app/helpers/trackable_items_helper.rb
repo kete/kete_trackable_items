e15cef073675ab170ba488d9912a16329de49485
Walter McGinnis <wm@waltermcginnis.com>
2012-05-11 11:15:37 +1200

    Adding trackable_item_shelf_locations#new route as we will use that to allow user to pick repository and shelf location. Fixing translation key stuff for button.

--- app/helpers/trackable_items_helper.rb
+++ app/helpers/trackable_items_helper.rb
@@ -2,20 +2,20 @@ module TrackableItemsHelper
   # wrapped in a div to allow for styling the form to be inline
   def button_to_place_in_shelf_location(trackable_item)
     "<div class=\"add-to-shelf-location\">" +
-        button_to(t('trackable_items_helper.add_to_shelf_location'),
-                  :controller => :trackable_items_shelf_locations,
-                  :action => :create) +
+        button_to(t('.add_to_shelf_location'),
+                  new_trackable_item_shelf_location_url) +
         "</div>"
   end
 
   def shelf_locations_or_appropriate_action(trackable_item)
     html = String.new
     if trackable_item.has_shelf_locations?
-      html = "<div class=\"shelf-locations\">" +
+      html += "<div class=\"shelf-locations\">" +
           "</div>"
-    else
-      html = button_to_place_in_shelf_location(trackable_item)
     end
+
+    html += button_to_place_in_shelf_location(trackable_item)
+
     html
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Routes needs more thought since some default routes in ArchivesCentral are taking precedence.
 ActionController::Routing::Routes.draw do |map|
   map.resources :tracked_items, :path_prefix => ':urlified_name', :only => [:create]
-  map.resources :trackable_item_shelf_locations, :path_prefix => ':urlified_name', :only => [:create]
+  map.resources :trackable_item_shelf_locations, :path_prefix => ':urlified_name', :only => [:new, :create]
   map.resources :repositories, :path_prefix => ':urlified_name', :has_many => [:shelf_locations, :tracking_lists]
 end

1a6c3b766d72471e1160ee72e9dac7640eb45d3a
Walter McGinnis <wm@waltermcginnis.com>
2012-05-11 10:46:51 +1200

    Renaming method name to be more accurate. Refactoring.

--- app/helpers/trackable_items_helper.rb
+++ app/helpers/trackable_items_helper.rb
@@ -2,16 +2,15 @@ module TrackableItemsHelper
   # wrapped in a div to allow for styling the form to be inline
   def button_to_place_in_shelf_location(trackable_item)
     "<div class=\"add-to-shelf-location\">" +
-        button_to(t('.add_to_shelf_location'),
+        button_to(t('trackable_items_helper.add_to_shelf_location'),
                   :controller => :trackable_items_shelf_locations,
                   :action => :create) +
-        #trackable_item.class.as_foreign_key_sym => trackable_item) +
         "</div>"
   end
 
-  def shelf_location_status_or_button_to_place_in_shelf_location(trackable_item)
+  def shelf_locations_or_appropriate_action(trackable_item)
     html = String.new
-    if current_user && current_user != false && trackable_item.has_shelf_location?
+    if trackable_item.has_shelf_locations?
       html = "<div class=\"shelf-locations\">" +
           "</div>"
     else

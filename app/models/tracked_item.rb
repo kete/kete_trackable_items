43de22abfa7bcd1b66d3f56be1284e5a72316f0c
Walter McGinnis <wm@waltermcginnis.com>
2012-05-17 13:56:01 +1200

    Adding validation, etc. for associations for tracked_item.

--- app/models/tracked_item.rb
+++ app/models/tracked_item.rb
@@ -4,6 +4,9 @@ class TrackedItem < ActiveRecord::Base
   
   delegate :description_for_tracked_item, :to => :trackable_item
 
+  validates_presence_of :tracking_list, :message => lambda { I18n.t('tracked_item.tracking_list_blank_or_does_not_match') }
+  validates_presence_of :trackable_item, :message => lambda { I18n.t('tracked_item.trackable_item_blank_or_does_not_match') }
+
   # needs to be unique to tracking_list
   validates_uniqueness_of :tracking_list_id, :scope => [:trackable_item_type, :trackable_item_id]
 

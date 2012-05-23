032221d843db4e36f6dabd379b60247f3f16c7de
Walter McGinnis <wm@waltermcginnis.com>
2012-05-23 21:37:37 +1200

    Adding initial configurable searching support.

--- rails/init.rb
+++ rails/init.rb
@@ -15,4 +15,53 @@ config.to_prepare do
     key = File.basename(ext_path, '.rb').to_sym
     Kete.add_code_to_extensions_for(key, Proc.new { Kernel.load(ext_path) })
   end
+
+  # this is for site specific defined search scopes
+  # i.e. you can place your own Kete site's extended fields set up dependent search definitions
+  # in a config and have them available to kete_trackable_items code
+  Kete.define_reader_method_as('trackable_item_scopes',
+                               YAML.load(IO.read(File.join(Rails.root, 'config/trackable_item_scopes.yml'))))
+  
+  Kete.trackable_item_scopes.each do |key, options|
+    klass = key.camelize.constantize
+    key = key.to_sym
+
+    if options['prerequisite_extensions'].present?
+      options['prerequisite_extensions'].each do |code|
+        extension = Proc.new {
+          klass.class_eval do
+            eval(code)
+          end
+        }
+
+        Kete.add_code_to_extensions_for(key, extension)
+      end
+    end
+
+    if options['search_scopes'].present?
+      if options['search_scopes']['always_within_scopes'].present?
+        options['search_scopes']['always_within_scopes'].each do |scope_key, scope_code|
+          extension = Proc.new {
+            klass.class_eval do
+              named_scope(scope_key.to_sym, eval(scope_code))
+            end
+          }
+
+          Kete.add_code_to_extensions_for(key, extension)
+        end
+      end
+
+      if options['search_scopes']['text_searches'].present?
+        options['search_scopes']['text_searches'].each do |scope_key, scope_code|
+          extension = Proc.new {
+            klass.class_eval do
+              named_scope(scope_key.to_sym, eval(scope_code))
+            end
+          }
+
+          Kete.add_code_to_extensions_for(key, extension)
+        end
+      end
+    end
+  end
 end

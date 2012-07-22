c15fe4a02e1bc2ebb251cf12355eb4719a72a227
Walter McGinnis <wm@waltermcginnis.com>
2012-07-22 21:16:39 +1200

    Changing display for items to not include reference, as they are usually split out into their own display per row.

--- lib/kete_trackable_items/extensions/models/topic.rb
+++ lib/kete_trackable_items/extensions/models/topic.rb
@@ -2,5 +2,5 @@
 Topic.class_eval do
   # adding trackable item functionality
   include KeteTrackableItems::TrackableItem
-  set_up_as_trackable_item :described_as => :reference_title_date_range
+  set_up_as_trackable_item :described_as => :title_date_range
 end

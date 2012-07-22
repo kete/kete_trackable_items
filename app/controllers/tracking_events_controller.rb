ef0ed575225de14b7fd700c2d71d775f805b9860
Walter McGinnis <wm@waltermcginnis.com>
2012-07-22 23:26:36 +1200

    Paginating display of tracking events listing for a historical item.

--- app/controllers/tracking_events_controller.rb
+++ app/controllers/tracking_events_controller.rb
@@ -3,13 +3,17 @@ class TrackingEventsController < ApplicationController
   # A copy of ApplicationController has been removed from the module tree but is still active!
   unloadable
 
-  include KeteTrackableItems::MatchingTrackableItemsControllerHelpers
+  include KeteTrackableItems::PaginateSetUp
 
   def index
+    set_page_variables
+
     if params[:historical_item_id] && params[:historical_item_type]
       @historical_item = params[:historical_item_type].constantize.find(params[:historical_item_id])
     end
     
-    @tracking_events = @historical_item ? @historical_item.tracking_events : TrackingEvents.all
+    @tracking_events = @historical_item ? @historical_item.tracking_events.paginate(@page_options) : TrackingEvents.all.paginate(@page_options)
+
+    set_results_variables(@tracking_events)
   end
 end
